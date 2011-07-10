04f6da677dfec9077bde521e0ae50ad4b501d23b
DOZEN <kyosuke6428@gmail.com>
2011-07-11 00:10:43 +0900

    ちょっと変更

--- function.php
+++ function.php
@@ -85,12 +85,15 @@ function translation($translation){
     return str_ireplace($roman_lib,$kana_lib,$translation);
 }
 
-function ime($word){
-    $word = urlencode($word);
-    $google = file_get_contents("http://www.google.com/transliterate?langpair=ja-Hira|ja&text={$word}");
-    $word = json_decode($google);
-    foreach ($word as $status){
-        $line .= $status['1']['0'];
+function ime($translation,$type){
+    if ($type != 'H'){
+        $translation = roman2kana($translation);
+    }
+    $translation = urlencode($translation);
+    $translation = file_get_contents("http://www.google.com/transliterate?langpair=ja-Hira|ja&text={$translation}");
+    $translation = json_decode($translation);
+    foreach ($translation as $status){
+    $line .= $status['1']['0'];
     }
     return $line;
 }
